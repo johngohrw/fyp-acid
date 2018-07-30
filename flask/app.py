bb5eeb64e7b9d15b9e72b54a7ee70a820a3d166c
TheGreatGaben <gdtan3@student.monash.edu>
2018-07-31 00:57:38 +0800

    sample flask server code

---
+++ flask/app.py
@@ -0,0 +1,20 @@
+from flask import *
+app = Flask(__name__);
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html");
+
+@app.route("/upload", methods=["POST"])
+def file_upload():
+    return "File received!", 201;
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return "404 NOT FOUND!", 404;
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return render_template("405_error.html"), 405
+
