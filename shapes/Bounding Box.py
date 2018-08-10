2e607066bd5e2102be435b380e2dc3561badff1b
unknown <ykyio1@student.monash.edu>
2018-08-10 12:15:06 +0800

    OMG I DID IT! LOOK MA! THE BOXES ARE SO CLEAN!

--- shapes/Bounding Box.py
+++ shapes/Bounding Box.py
@@ -2,33 +2,44 @@ import cv2
 import numpy as np 
 
 file_name = '3.jpg'
- 
+
+# =============================== READ FILE ===================================
 # read and scale down image
 #img = cv2.pyrDown(cv2.imread(file_name, cv2.IMREAD_UNCHANGED))
 
 img = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
 
+# ============================== Pre-process ==================================
 noise_removal = cv2.GaussianBlur(img, (5, 5), 0)
 
 
 # Gray scale image
 grayscaled = cv2.cvtColor(noise_removal, cv2.COLOR_BGR2GRAY)
+#grayscaled = np.float32(grayscaled)
 #grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 # threshold image
-# ret, threshed_img = cv2.threshold(grayscaled, 127, 255, cv2.THRESH_BINARY)
+ret, threshed_img = cv2.threshold(grayscaled, 127, 255, cv2.THRESH_BINARY)
 
 # find contours and get the external one
-image, contours, hier = cv2.findContours(grayscaled, cv2.RETR_TREE,
+image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE,
                 cv2.CHAIN_APPROX_SIMPLE)
 
+# ============================ Get Contour / Box =============================
 # Draw only contours i want
 for contour in contours:
     area = cv2.contourArea(contour)
+    epsilon = 0.1*cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
 
-    if area > 1000:
-        cv2.drawContours(img, contour, -1, (0, 255, 0), 3)
+    if ((area >6000) and area <50000):
+        #cv2.drawContours(img, contour, -1, (0, 255, 0), 3)
+        x, y, w, h = cv2.boundingRect(approx)
+        # draw a green rectangle to visualize the bounding rect
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print(len(contours))
+        cv2.drawContours(img, approx, -1, (255, 255, 0), 1)
 
 
 
@@ -36,14 +47,14 @@ for contour in contours:
 # with each contour, draw boundingRect in green
 # a minAreaRect in red and
 # a minEnclosingCircle in blue
-
-for c in contours:
+    '''
+    for c in contours:
 
     # get the bounding rect
     x, y, w, h = cv2.boundingRect(c)
     # draw a green rectangle to visualize the bounding rect
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+    '''
     '''
     # get the min area rect
     rect = cv2.minAreaRect(c)
@@ -64,24 +75,18 @@ for c in contours:
     img = cv2.circle(img, center, radius, (255, 0, 0), 2)
     '''
 
-print(len(contours))
-cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
- 
+#print(len(contours))
+#cv2.drawContours(img, approx, -1, (255, 255, 0), 1)
+
+#=============================== SHOWING WINDOWS ============================
 cv2.imshow("contours", img)
 #cv2.imshow("Threshold", threshed_img)
 #cv2.imshow("GrayScaled", grayscaled)
 #cv2.imshow("HSV", hsv)
 
+#=============================== Interface for the program ==================
 ESC = 27
 
 cv2.waitKey(0)
 
-'''
-while True:
-    keycode = cv2.waitKey()
-    if keycode != -1:
-        keycode &amp;= 0xFF
-        if keycode == ESC:
-            break
-'''
 cv2.destroyAllWindows()
