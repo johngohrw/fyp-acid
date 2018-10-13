a62fb80ca5b820b6bc32ae2c14c71d91ec689c01
John Goh <john@accendo.com.my>
2018-10-13 23:39:41 +0800

    added trippy bg on frontend

--- fe-dev/src/App.js
+++ fe-dev/src/App.js
@@ -8,6 +8,9 @@ import TemplateTestPage from './pages/TemplateTestPage';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './css/app.css';
+import './css/trippycircles.css';
+import './css/trippybourbon.css';
+
 
 class App extends Component {
 
@@ -21,6 +24,66 @@ class App extends Component {
         <CssBaseline />
         <Router>
           <div className="main-container container">  
+            <div className="trippybg">
+        
+              
+{/* <div className="bourbon-container">
+  <div className="bourbon-ring el-1"></div>
+  <div className="bourbon-ring el-2"></div>
+  <div className="bourbon-ring el-3"></div>
+  <div className="bourbon-ring el-4"></div>
+  <div className="bourbon-ring el-5"></div>
+  <div className="bourbon-ring el-6"></div>
+  <div className="bourbon-ring el-7"></div>
+  <div className="bourbon-ring el-8"></div>
+  <div className="bourbon-ring el-9"></div>
+  <div className="bourbon-ring el-10"></div>
+  <div className="bourbon-ring el-11"></div>
+  <div className="bourbon-ring el-12"></div>
+  <div className="bourbon-ring el-13"></div>
+  <div className="bourbon-ring el-14"></div>
+  <div className="bourbon-ring el-15"></div>
+  <div className="bourbon-ring el-16"></div>
+  <div className="bourbon-ring el-17"></div>
+  <div className="bourbon-ring el-18"></div>
+  <div className="bourbon-ring el-19"></div>
+  <div className="bourbon-ring el-20"></div>
+  <div className="bourbon-ring el-21"></div>
+  <div className="bourbon-ring el-22"></div>
+  <div className="bourbon-ring el-23"></div>
+  <div className="bourbon-ring el-24"></div>
+  <div className="bourbon-ring el-25"></div>
+  <div className="bourbon-ring el-26"></div>
+  <div className="bourbon-ring el-27"></div>
+  <div className="bourbon-ring el-28"></div>
+  <div className="bourbon-ring el-29"></div>
+  <div className="bourbon-ring el-30"></div>
+</div> */}
+
+<ul class="trippycontainer">
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+  <li class="trippycircle"></li>
+</ul>
+
+            </div>
             <AppHeader />
             <div className="content">
               <Route exact path='/' component={WelcomePage}/>
