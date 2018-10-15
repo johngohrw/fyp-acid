8771d1f2e6ff965c4eca67ebd9ef8d23ccc05708
TheGreatGaben <gdtan3@student.monash.edu>
2018-10-15 21:39:17 +0800

    some refactoring and reports yay

--- train_test.py
+++ train_test.py
@@ -2,8 +2,7 @@ import os
 
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
@@ -12,14 +11,9 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 
-if __name__ == "__main__":
-    dataFilename = "data.csv";
-    labelFilename = "labels.txt";
-    # Data or label file does not exist then just extract the features, give
-    # label and write them to their respective files
-    if not os.path.isfile(dataFilename) or not os.path.isfile(labelFilename):
-        print("No data CSV file or label file");
-    else:
+class ACIDTrainTest:
+
+    def __init__(self, dataFilename, labelFilename):
         npData = np.genfromtxt(dataFilename, delimiter=",");
         npLabels = np.genfromtxt(labelFilename, delimiter="\n", dtype=np.uint8);
         assert npData.shape[0] == npLabels.shape[0];
@@ -32,10 +26,28 @@ if __name__ == "__main__":
         #   train_size: float, int (between 0.0 and 1.0)
         #   random_state: A fixed seed value to give to the PRNG
         #
-        # Split the dataset to train+validation and test first
-        X_train, X_test, y_train, y_test = train_test_split(npData, npLabels, random_state=0);
+        # X denotes the features/data while y denotes the class labels
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(npData, npLabels, random_state=0);
+        print("\nTraining set size: {}".format(self.y_train.shape[0]));
+        print("Test set size: {}".format(self.y_test.shape[0]));
 
-        # Performing k fold cross validation (5 folds)
+        train_comp_count = np.count_nonzero(self.y_train);
+        train_comp_percent = train_comp_count/self.y_train.shape[0];
+        train_nocomp_percent = 1 - train_comp_percent;
+        print("Compound images in training set: {:.2f}".format(train_comp_percent));
+        print("Non-compound images in training set: {:.2f}".format(train_nocomp_percent));
+
+        test_comp_count = np.count_nonzero(self.y_test);
+        test_comp_percent = test_comp_count/self.y_test.shape[0];
+        test_nocomp_percent = 1 - test_comp_percent;
+        print("Compound images in test set: {:.2f}".format(test_comp_percent));
+        print("Non-compound images in test set: {:.2f}\n".format(test_nocomp_percent));
+
+
+    def paramTestCrossValidate(self, estimator, model_name, param_grid, cross_k):
+        #
+        # Grid search (aka parameter testing) and cross validation
+        #
         # NOTE: Cross validation is not used to build a model for
         # new data. K models are build when running a k fold cross validation
         # so it just evaluate how well the model will generalize when
@@ -58,46 +70,14 @@ if __name__ == "__main__":
         #             to be in the training set, rather than having some in
         #             training and some in testing.
 
-        # Grid search (aka parameter testing) and cross validation
-        cross_k = 5;
-
-        # Testing best parameters for linear SVM
-        linear_param_grid = {"C": [0.001, 0.01, 0.1, 1, 10, 100]};
-        linear_grid_search = GridSearchCV(LinearSVC(random_state=0), linear_param_grid, cv=cross_k);
-        linear_grid_search.fit(X_train, y_train);
-        print("Linear SVM");
+        grid_search = GridSearchCV(estimator, param_grid, cv=cross_k);
+        grid_search.fit(self.X_train, self.y_train);
+        print("{}".format(model_name));
         print("===========");
-        print("Linear SVM best cross-validation score: {:.2f}".format(linear_grid_search.best_score_));
-        print("Linear SVM best parameters: {}".format(linear_grid_search.best_params_));
-        print("Linear SVM Train set score: {:.2f}".format(linear_grid_search.score(X_train, y_train)));
-        print("Linear SVM Test set predictions:\n{}".format(linear_grid_search.predict(X_test)));
-        print("Linear SVM Test set score: {:.2f}\n".format(linear_grid_search.score(X_test, y_test)));
-
-        # Testing the best parameters for RBF kernel SVM
-        kernel_param_grid = {"gamma": [0.001, 0.01, 0.1, 1, 10, 100],
-                "C": [0.001, 0.01, 0.1, 1, 10, 100]};
-        kernel_grid_search = GridSearchCV(SVC(random_state=0), kernel_param_grid, cv=cross_k);
-        kernel_grid_search.fit(X_train, y_train);
-        print("RBF Kernel SVM");
-        print("==============");
-        print("RBF Kernel SVM best cross-validation score: {:.2f}".format(kernel_grid_search.best_score_));
-        print("RBF Kernel SVM best parameters: {}".format(kernel_grid_search.best_params_));
-        print("RBF Kernel SVM Train set score: {:.2f}".format(kernel_grid_search.score(X_train, y_train)));
-        print("RBF Kernel SVM Test set predictions:\n{}".format(kernel_grid_search.predict(X_test)));
-        print("RBF Kernel SVM Test score: {:.2f}\n".format(kernel_grid_search.score(X_test, y_test)));
-
-        # Testing the best parameters for KNN
-        knn_param_grid = {"n_neighbors": [1, 2, 3]};
-        knn_grid_search = GridSearchCV(KNeighborsClassifier(), knn_param_grid, cv=cross_k);
-        knn_grid_search.fit(X_train, y_train);
-        print("K nearest neighbours");
-        print("====================");
-        print("KNN best cross-validation score: {:.2f}".format(knn_grid_search.best_score_));
-        print("KNN best parameters: {}".format(knn_grid_search.best_params_));
-        print("KNN Train set score: {:.2f}".format(knn_grid_search.score(X_train, y_train)));
-        print("KNN Test set predictions:\n{}".format(knn_grid_search.predict(X_test)));
-        print("KNN Test set score: {:.2f}\n".format(knn_grid_search.score(X_test, y_test)));
-
+        print("{} best cross-validation score: {:.2f}".format(model_name, grid_search.best_score_));
+        print("{} best parameters: {}".format(model_name, grid_search.best_params_));
+        print("{} Train set score: {:.2f}".format(model_name, grid_search.score(self.X_train, self.y_train)));
+        print("{} Test set score: {:.2f}\n".format(model_name, grid_search.score(self.X_test, self.y_test)));
 
         # Confusion matrix is a 2x2 matrix, where row is the true class
         # while column is the predicted class
@@ -107,8 +87,9 @@ if __name__ == "__main__":
         #                   FN = False Negative (Predicted 0 but actually 1)
         #                   TP = True Positive (Predicted 1 and is actually 1)
         #
-        #confusion = confusion_matrix(y_test, y_pred);
-        #print("Kernel SVM confustion matrix:\n{}".format(confusion));
+        y_pred = grid_search.predict(self.X_test);
+        confusion = confusion_matrix(self.y_test, y_pred);
+        print("{} confusion matrix:\n{}".format(model_name, confusion));
 
         # Summarizing the confustion matrix
         #
@@ -120,6 +101,33 @@ if __name__ == "__main__":
         # f1-score (F) = 2 * precision * recall
         #                   --------------------
         #                    precision + recall
-        #print(y_test);
-        #print(classification_report(y_test, y_pred, target_names=["not-compound", "compound"]));
+        print(classification_report(self.y_test, y_pred, target_names=["non-compound", "compound"]));
+        print();
+
+
+if __name__ == "__main__":
+    dataFilename = "data.csv";
+    labelFilename = "labels.txt";
+    if not os.path.isfile(dataFilename) or not os.path.isfile(labelFilename):
+        print("No data CSV file or label file");
+    else:
+        acid = ACIDTrainTest(dataFilename, labelFilename);
+
+        # Stratified version ensures that the percentages of the samples
+        # for each class (compound and non-compound) are preserved in
+        # each fold, preventing an imbalance distribution
+        cross_k = StratifiedKFold(n_splits=5);
+
+        # Testing best parameters for linear SVM
+        linear_param_grid = {"C": [0.001, 0.01, 0.1, 1, 10, 100]};
+        acid.paramTestCrossValidate(LinearSVC(random_state=0), "Linear SVM", linear_param_grid, cross_k);
+
+        # Testing the best parameters for RBF kernel SVM
+        kernel_param_grid = {"gamma": [0.001, 0.01, 0.1, 1, 10, 100],
+                "C": [0.001, 0.01, 0.1, 1, 10, 100]};
+        acid.paramTestCrossValidate(SVC(random_state=0), "RBF Kernel SVM", kernel_param_grid, cross_k);
+
+        # Testing the best parameters for KNN
+        knn_param_grid = {"n_neighbors": [1, 2, 3]};
+        acid.paramTestCrossValidate(KNeighborsClassifier(), "KNN", knn_param_grid, cross_k);
 
