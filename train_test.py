798b0b85690dc16b693d123aab465cff89644897
TheGreatGaben <gdtan3@student.monash.edu>
2018-10-15 18:59:05 +0800

    parameter testing with svm and knn

--- train_test.py
+++ train_test.py
@@ -24,39 +24,16 @@ if __name__ == "__main__":
         npLabels = np.genfromtxt(labelFilename, delimiter="\n", dtype=np.uint8);
         assert npData.shape[0] == npLabels.shape[0];
 
-        # Data has 'X' as prefix while the labels have 'y' as prefix
+        # Training and testing set
         #
-        # This method randomly shuffles the data and labels for us.
+        # The train_test_split method randomly shuffles the data and labels for us.
         # Potentially useful params:
         #   test_size: float, int (between 0.0 and 1.0), default = 0.25
         #   train_size: float, int (between 0.0 and 1.0)
         #   random_state: A fixed seed value to give to the PRNG
         #
+        # Split the dataset to train+validation and test first
         X_train, X_test, y_train, y_test = train_test_split(npData, npLabels, random_state=0);
-        print(X_train);
-        print(X_test);
-        print(y_train);
-        print(y_test);
-
-        # Training with KNN (2 neighbor classifier)
-        knn = KNeighborsClassifier(n_neighbors=2);
-        knn.fit(X_train, y_train);
-        print("KNN Train set score: {:.2f}".format(knn.score(X_train, y_train)));
-
-        # Testing with KNN
-        y_pred = knn.predict(X_test);
-        print("KNN Test set predictions:\n{}".format(y_pred));
-        print("KNN Test set score: {:.2f}".format(knn.score(X_test, y_test)));
-
-        # Training with SVM
-        svm = LinearSVC(random_state=0, C=750);
-        svm.fit(X_train, y_train);
-        print("SVM Train set score: {:.2f}".format(svm.score(X_train, y_train)));
-
-        # Testing with SVM
-        svm_y_pred = svm.predict(X_test);
-        print("SVM Test set predictions:\n{}".format(svm_y_pred));
-        print("SVM Test set score: {:.2f}".format(svm.score(X_test, y_test)));
 
         # Performing k fold cross validation (5 folds)
         # NOTE: Cross validation is not used to build a model for
@@ -80,35 +57,47 @@ if __name__ == "__main__":
         #             emotions, its better that all the images of that person
         #             to be in the training set, rather than having some in
         #             training and some in testing.
-        knn2 = KNeighborsClassifier(n_neighbors=2);
-        kfold = KFold(n_splits=5, shuffle=True, random_state=0);
-        scores = cross_val_score(knn2, npData, npLabels, cv=kfold);
-        print("Cross validation scores: {}".format(scores));
-        print("Average cross-validation score: {:.2f}".format(scores.mean()));
 
-        # Training, testing and validation set
-        #
-        # Split the dataset to train+validation and test first
-        X_trainval, X_test, y_trainval, y_test = train_test_split(npData, npLabels, random_state=0);
-        # Then split the train+validation set to train and validation set
-        # accordingly
-        X_train, X_valid, y_train, y_valid = train_test_split(X_trainval, y_trainval, random_state=1);
+        # Grid search (aka parameter testing) and cross validation
+        cross_k = 5;
 
-        # Or we can do all that with a single class
-        param_grid = {"gamma": [0.001, 0.01, 0.1, 1, 10, 100],
+        # Testing best parameters for linear SVM
+        linear_param_grid = {"C": [0.001, 0.01, 0.1, 1, 10, 100]};
+        linear_grid_search = GridSearchCV(LinearSVC(random_state=0), linear_param_grid, cv=cross_k);
+        linear_grid_search.fit(X_train, y_train);
+        print("Linear SVM");
+        print("===========");
+        print("Linear SVM best cross-validation score: {:.2f}".format(linear_grid_search.best_score_));
+        print("Linear SVM best parameters: {}".format(linear_grid_search.best_params_));
+        print("Linear SVM Train set score: {:.2f}".format(linear_grid_search.score(X_train, y_train)));
+        print("Linear SVM Test set predictions:\n{}".format(linear_grid_search.predict(X_test)));
+        print("Linear SVM Test set score: {:.2f}\n".format(linear_grid_search.score(X_test, y_test)));
+
+        # Testing the best parameters for RBF kernel SVM
+        kernel_param_grid = {"gamma": [0.001, 0.01, 0.1, 1, 10, 100],
                 "C": [0.001, 0.01, 0.1, 1, 10, 100]};
-        grid_search = GridSearchCV(SVC(), param_grid, cv=5);
-        grid_search.fit(X_trainval, y_trainval);
+        kernel_grid_search = GridSearchCV(SVC(random_state=0), kernel_param_grid, cv=cross_k);
+        kernel_grid_search.fit(X_train, y_train);
         print("RBF Kernel SVM");
-        print("Best cross-validation score: {:.2f}".format(grid_search.best_score_));
-        print("Best parameters: {}".format(grid_search.best_params_));
-        print("Test score with best parameters: {:.2f}".format(grid_search.score(X_test, y_test)));
+        print("==============");
+        print("RBF Kernel SVM best cross-validation score: {:.2f}".format(kernel_grid_search.best_score_));
+        print("RBF Kernel SVM best parameters: {}".format(kernel_grid_search.best_params_));
+        print("RBF Kernel SVM Train set score: {:.2f}".format(kernel_grid_search.score(X_train, y_train)));
+        print("RBF Kernel SVM Test set predictions:\n{}".format(kernel_grid_search.predict(X_test)));
+        print("RBF Kernel SVM Test score: {:.2f}\n".format(kernel_grid_search.score(X_test, y_test)));
 
-        # Rebuild the model using the best parameters
-        kernel_svm = SVC(**grid_search.best_params_);
-        kernel_svm.fit(X_trainval, y_trainval);
-        y_pred = kernel_svm.predict(X_test);
-        test_score = kernel_svm.score(X_test, y_test);
+        # Testing the best parameters for KNN
+        knn_param_grid = {"n_neighbors": [1, 2, 3]};
+        knn_grid_search = GridSearchCV(KNeighborsClassifier(), knn_param_grid, cv=cross_k);
+        knn_grid_search.fit(X_train, y_train);
+        print("K nearest neighbours");
+        print("====================");
+        print("KNN best cross-validation score: {:.2f}".format(knn_grid_search.best_score_));
+        print("KNN best parameters: {}".format(knn_grid_search.best_params_));
+        print("KNN Train set score: {:.2f}".format(knn_grid_search.score(X_train, y_train)));
+        print("KNN Test set predictions:\n{}".format(knn_grid_search.predict(X_test)));
+        print("KNN Test set score: {:.2f}\n".format(knn_grid_search.score(X_test, y_test)));
+
 
         # Confusion matrix is a 2x2 matrix, where row is the true class
         # while column is the predicted class
@@ -118,8 +107,8 @@ if __name__ == "__main__":
         #                   FN = False Negative (Predicted 0 but actually 1)
         #                   TP = True Positive (Predicted 1 and is actually 1)
         #
-        confusion = confusion_matrix(y_test, y_pred);
-        print("Kernel SVM confustion matrix:\n{}".format(confusion));
+        #confusion = confusion_matrix(y_test, y_pred);
+        #print("Kernel SVM confustion matrix:\n{}".format(confusion));
 
         # Summarizing the confustion matrix
         #
@@ -131,6 +120,6 @@ if __name__ == "__main__":
         # f1-score (F) = 2 * precision * recall
         #                   --------------------
         #                    precision + recall
-        print(y_test);
-        print(classification_report(y_test, y_pred, target_names=["not-compound", "compound"]));
+        #print(y_test);
+        #print(classification_report(y_test, y_pred, target_names=["not-compound", "compound"]));
 
