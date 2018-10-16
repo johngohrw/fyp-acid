1dca8fbbf06d03c952dc9d1a67922f5679056b5d
TheGreatGaben <gdtan3@student.monash.edu>
2018-10-16 16:46:03 +0800

    integrate shape features

--- features.py
+++ features.py
@@ -7,16 +7,18 @@ import numpy as np
 
 import sys
 sys.path.append("./ocr/");
+sys.path.append("./shapes/");
 sys.path.append("./lbp/");
 
 from modules.lbp import LocalBinaryPatterns
 from modules.subdivisions import subdivide_checkeredLBP, divisions
 from modules.shifting import rightshift, bottomshift
 from lbp2 import getLBPHistogram
+from Method2 import Shapes
 from ocr import OCR
 
 
-def getFeatures(imgNames, imgDir, lbp, ocr, dataFile, labelsFile, label_value, limit = None):
+def getFeatures(imgNames, imgDir, lbp, shape, ocr, dataFile, labelsFile, label_value, limit = None):
     imgLimit = limit;
     if limit == None:
         imgLimit = len(imgNames);
@@ -31,10 +33,18 @@ def getFeatures(imgNames, imgDir, lbp, ocr, dataFile, labelsFile, label_value, l
         img = cv2.imread(imgPath);
         lbpHist = getLBPHistogram(lbp, img);
         lbpHistStr = np.array2string(lbpHist, max_line_width=math.inf, precision=5, threshold=100000,formatter={'float_kind':lambda x: "%.5f" % x}, separator=',', suppress_small=False)[1:-1];
+
+        # Shape features
+        shapeFeatures = shape.boxFeatures(img);
+
         # OCR feature
         _, frequency = ocr.recognize(img);
 
-        features = "{},{}\n".format(lbpHistStr, frequency);
+        features = lbpHistStr;
+        for area in shapeFeatures:
+            features += ",{:.5f}".format(area);
+
+        features = "{},{}\n".format(features, frequency);
         print(features);
         dataFile.write(features);
         labelsFile.write("{}\n".format(label_value));
@@ -64,9 +74,10 @@ if __name__ == "__main__":
         print(len(noCompImgs));
 
         lbp = LocalBinaryPatterns(8, 24, "uniform") #number of points, radius
+        shape = Shapes();
         ocr = OCR();
 
         with open(dataFilename, "w") as dataFile, open(labelFilename, "w") as labelsFile:
-            getFeatures(compImgs, compPath, lbp, ocr, dataFile, labelsFile, 1, limit = comp_size);
-            getFeatures(noCompImgs, noCompPath, lbp, ocr, dataFile, labelsFile, 0, limit = nocomp_size);
+            getFeatures(compImgs, compPath, lbp, shape, ocr, dataFile, labelsFile, 1, limit = comp_size);
+            getFeatures(noCompImgs, noCompPath, lbp, shape, ocr, dataFile, labelsFile, 0, limit = nocomp_size);
 
--- shapes/Method2.py
+++ shapes/Method2.py
@@ -4,36 +4,32 @@ import numpy as np
 
 class Shapes:
 
-    def __init__ (self, imageName):
-        currentDir = os.path.dirname(os.path.realpath(__file__));
-        img_path = os.path.join(currentDir, imageName )
-
-        self.image = cv.imread(img_path, 0)
-        self.imgHeight = self.image.shape[0]
-        self.imgWidth = self.image.shape[1]
-        
+    def __init__ (self):
+        self.image = None;
+        self.imgHeight = None;
+        self.imgWidth = None;
 
 
     def sort_contours(self, cnts, method="left-to-right"):
         # initialize the reverse flag and sort index
         reverse = False
         i = 0
-    
+
         # handle if we need to sort in reverse
         if method == "right-to-left" or method == "bottom-to-top":
             reverse = True
-    
+
         # handle if we are sorting against the y-coordinate rather than
         # the x-coordinate of the bounding box
         if method == "top-to-bottom" or method == "bottom-to-top":
             i = 1
-    
+
         # construct the list of bounding boxes and sort them from top to
         # bottom
         boundingBoxes = [cv.boundingRect(c) for c in cnts]
         (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
             key=lambda b:b[1][i], reverse=reverse))
-    
+
         # return the list of sorted contours and bounding boxes
         return (cnts, boundingBoxes)
 
@@ -43,23 +39,24 @@ class Shapes:
 
     def binarizeImage (self, image):
         (thresh, binarized) = cv.threshold(image, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)  # Thresholding the image
-        
+
         return binarized
 
     def invertImage (self, image):
-        inv_img = 255 - image 
-        
+        inv_img = 255 - image
+
         return inv_img
 
     def medianBlurImage (self, image):
         kernel_size = 3
         median_blur = cv.medianBlur(image, kernel_size)
         #cv.imshow('blurry', median_blur)
-        
+
         return median_blur
 
     def preProcess(self):
-        img_blur = self.medianBlurImage(self.image)
+        gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY);
+        img_blur = self.medianBlurImage(gray);
         img_binarized = self.binarizeImage(img_blur)
         img_inverted = self.invertImage(img_binarized)
         return img_inverted
@@ -68,15 +65,19 @@ class Shapes:
         img_area = self.imgHeight * self.imgWidth
         return img_area
 
-    def boxFeatures(self):
+    def boxFeatures(self, img, output_limit = 8):
+        self.image = img
+        self.imgHeight = self.image.shape[0]
+        self.imgWidth = self.image.shape[1]
+
         processed = self.preProcess()
 
         # Defining a kernel length
         kernel_length = np.array(self.image).shape[1]//40
-        
+
         # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
         verticle_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernel_length))
-        
+
         # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
         hori_kernel = cv.getStructuringElement(cv.MORPH_RECT, (kernel_length, 1))
 
@@ -99,14 +100,14 @@ class Shapes:
         img_final_bin = cv.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
         img_final_bin = cv.erode(~img_final_bin, kernel, iterations=2)
         (thresh, img_final_bin) = cv.threshold(img_final_bin, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-   
+
         # Find contours for image, which will detect all the boxes
         im2, contours, hierarchy = cv.findContours(
             img_final_bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         # Sort all the contours by top to bottom.
         (contours, boundingBoxes) = self.sort_contours(contours, method="top-to-bottom")
-        
+
         threshed_height = self.imgHeight *0.3
         threshed_width = self.imgWidth * 0.3
 
@@ -120,22 +121,31 @@ class Shapes:
             contour_area = w*h
             area_ratio = contour_area / self.getImageArea()
             featureArray.append(area_ratio)
-        
+            if len(featureArray) == output_limit:
+                break;
+
+        while len(featureArray) < output_limit:
+            featureArray.append(0);
+
         return featureArray
+
 
 def writeToFile (toWrite):
     resultArea = open('resultShape.txt', 'w')
-    resultingArea.write(str(the_features))
-    resultingArea.write('\n')
+    resultArea.write(str(the_features))
+    resultArea.write('\n')
     resultArea.close()
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    currentDir = os.path.dirname(os.path.realpath(__file__));
     img_name = "Images/" + str(1) + ".jpg"
+    img_path = os.path.join(currentDir, img_name )
 
-    shape = Shapes(img_name)
-    the_features = shape.boxFeatures()
+    image = cv.imread(img_path)
+    shape = Shapes();
+    the_features = shape.boxFeatures(image)
 
     writeToFile(the_features)
-            
-    
+
+
 
