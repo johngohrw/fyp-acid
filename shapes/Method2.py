5f665a09e097c9e30333d2615c4e5f0672109d0a
unknown <ykyio1@student.monash.edu>
2018-10-16 15:25:12 +0800

    Vely Naise Classy

--- shapes/Method2.py
+++ shapes/Method2.py
@@ -2,162 +2,140 @@ import os
 import cv2 as cv
 import numpy as np
 
-def sort_contours(cnts, method="left-to-right"):
-	# initialize the reverse flag and sort index
-	reverse = False
-	i = 0
- 
-	# handle if we need to sort in reverse
-	if method == "right-to-left" or method == "bottom-to-top":
-		reverse = True
- 
-	# handle if we are sorting against the y-coordinate rather than
-	# the x-coordinate of the bounding box
-	if method == "top-to-bottom" or method == "bottom-to-top":
-		i = 1
- 
-	# construct the list of bounding boxes and sort them from top to
-	# bottom
-	boundingBoxes = [cv.boundingRect(c) for c in cnts]
-	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-		key=lambda b:b[1][i], reverse=reverse))
- 
-	# return the list of sorted contours and bounding boxes
-	return (cnts, boundingBoxes)
+class Shapes:
 
-def getImageHeightWidth (image):
-    height, width = image.shape[:2]
-    return height, width
+    def __init__ (self, imageName):
+        currentDir = os.path.dirname(os.path.realpath(__file__));
+        img_path = os.path.join(currentDir, imageName )
 
-def binarizeImage (image):
-    (thresh, binarized) = cv.threshold(image, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)  # Thresholding the image
-    return binarized
-
-def invertImage (image):
-    inv_img = 255 - image 
-    return inv_img
-
-def medianBlurImage (image):
-    kernel_size = 3
-    median_blur = cv.medianBlur(image, kernel_size)
-    #cv.imshow('blurry', median_blur)
-    return median_blur
-
-def detectBoxes(img, resultingArea):
-    currentDir = os.path.dirname(os.path.realpath(__file__));
-
-    img_blur = medianBlurImage(img)
-
-    img_binarized = binarizeImage(img_blur)
-    img_inverted = invertImage(img_binarized)
-
-    filename = "Image_bin.jpg"
-    img_path = os.path.join(currentDir, filename)
-    cv.imwrite(img_path, img_inverted)
-   
-    # Defining a kernel length
-    kernel_length = np.array(img).shape[1]//40
-    
-    # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
-    verticle_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernel_length))
-    
-    # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
-    hori_kernel = cv.getStructuringElement(cv.MORPH_RECT, (kernel_length, 1))
-
-    # A kernel of (3 X 3) ones.
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
-
-    # Morphological operation to detect verticle lines from an image
-    img_temp1 = cv.erode(img_inverted, verticle_kernel, iterations=3)
-    verticle_lines_img = cv.dilate(img_temp1, verticle_kernel, iterations=3)
-
-    filename = "verticle_lines.jpg"
-    img_path = os.path.join(currentDir, filename)
-    cv.imwrite(img_path, verticle_lines_img)
-
-    # Morphological operation to detect horizontal lines from an image
-    img_temp2 = cv.erode(img_inverted, hori_kernel, iterations=3)
-    horizontal_lines_img = cv.dilate(img_temp2, hori_kernel, iterations=3)
-
-    filename = "horizontal_lines.jpg"
-    img_path = os.path.join(currentDir, filename)
-    cv.imwrite(img_path, horizontal_lines_img)
-
-    # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
-    alpha = 0.5
-    beta = 1.0 - alpha
-
-    # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
-    img_final_bin = cv.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
-    img_final_bin = cv.erode(~img_final_bin, kernel, iterations=2)
-    (thresh, img_final_bin) = cv.threshold(img_final_bin, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-
-    # For Debugging
-    # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
-    filename = "img_final_bin.jpg"
-    img_path = os.path.join(currentDir, filename)
-    cv.imwrite(img_path, img_final_bin)
-    
-    # Find contours for image, which will detect all the boxes
-    im2, contours, hierarchy = cv.findContours(
-        img_final_bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-
-    # Sort all the contours by top to bottom.
-    (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
-    
-    img_height, img_width = getImageHeightWidth(img)
-    threshed_height = img_height *0.3
-    threshed_width = img_width * 0.3
-
-    img_area = img_height * img_width
-
-    '''
-    highest_height = img_height
-    highest_width = img_width
-    '''
- 
-    for c in contours:
-        # Returns the location and width,height for every contour
-        x, y, w, h = cv.boundingRect(c)
+        self.image = cv.imread(img_path, 0)
+        self.imgHeight = self.image.shape[0]
+        self.imgWidth = self.image.shape[1]
         
-        #print(w)
-        #print(h)
-        cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        #cv.imshow('drawing image', image)
-        #cv.waitKey(0)
-        '''
-    # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
-        #if (w > 80 and h > 20) and w > 3*h:
-        if (w < img_width or h < img_height):
-            if (w > threshed_width  and h > threshed_height):
-                new_img = img[y:y+h, x:x+w]
-                cv.imshow('cropped images', new_img)
-                contour_area = w*h
-                #print(area) #area
-                #cv.waitKey(0)
-
-                area_ratio = contour_area / img_area
-                resultingArea.write(str(area_ratio) + ', ')
-        '''
-        contour_area = w*h
-        area_ratio = contour_area / img_area
-        resultingArea.write(str(area_ratio) + ', ')
-
-    resultingArea.write('\n')
 
 
-currentDir = os.path.dirname(os.path.realpath(__file__));
-
-resultArea = open('resultShape.txt', 'w')
-for i in range (1, 32):
-    img_name = "Images/" + str(i) + ".jpg"
-    img_path = os.path.join(currentDir, img_name )
-    image = cv.imread(img_path, 0)
-    #cv.imshow(img_name, image)
-    #cv.waitKey(0)
+    def sort_contours(self, cnts, method="left-to-right"):
+        # initialize the reverse flag and sort index
+        reverse = False
+        i = 0
     
-    detectBoxes(image, resultArea)
-    cv.destroyAllWindows()
-resultArea.close()
+        # handle if we need to sort in reverse
+        if method == "right-to-left" or method == "bottom-to-top":
+            reverse = True
+    
+        # handle if we are sorting against the y-coordinate rather than
+        # the x-coordinate of the bounding box
+        if method == "top-to-bottom" or method == "bottom-to-top":
+            i = 1
+    
+        # construct the list of bounding boxes and sort them from top to
+        # bottom
+        boundingBoxes = [cv.boundingRect(c) for c in cnts]
+        (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+            key=lambda b:b[1][i], reverse=reverse))
+    
+        # return the list of sorted contours and bounding boxes
+        return (cnts, boundingBoxes)
+
+    def getImageHeightWidth (self, image):
+        height, width = image.shape[:2]
+        return height, width
+
+    def binarizeImage (self, image):
+        (thresh, binarized) = cv.threshold(image, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)  # Thresholding the image
+        
+        return binarized
+
+    def invertImage (self, image):
+        inv_img = 255 - image 
+        
+        return inv_img
+
+    def medianBlurImage (self, image):
+        kernel_size = 3
+        median_blur = cv.medianBlur(image, kernel_size)
+        #cv.imshow('blurry', median_blur)
+        
+        return median_blur
+
+    def preProcess(self):
+        img_blur = self.medianBlurImage(self.image)
+        img_binarized = self.binarizeImage(img_blur)
+        img_inverted = self.invertImage(img_binarized)
+        return img_inverted
+
+    def getImageArea(self):
+        img_area = self.imgHeight * self.imgWidth
+        return img_area
+
+    def boxFeatures(self):
+        processed = self.preProcess()
+
+        # Defining a kernel length
+        kernel_length = np.array(self.image).shape[1]//40
+        
+        # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
+        verticle_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernel_length))
+        
+        # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
+        hori_kernel = cv.getStructuringElement(cv.MORPH_RECT, (kernel_length, 1))
+
+        # A kernel of (3 X 3) ones.
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+
+        # Morphological operation to detect verticle lines from an image
+        img_temp1 = cv.erode(processed, verticle_kernel, iterations=3)
+        verticle_lines_img = cv.dilate(img_temp1, verticle_kernel, iterations=3)
+
+        # Morphological operation to detect horizontal lines from an image
+        img_temp2 = cv.erode(processed, hori_kernel, iterations=3)
+        horizontal_lines_img = cv.dilate(img_temp2, hori_kernel, iterations=3)
+
+        # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
+        alpha = 0.5
+        beta = 1.0 - alpha
+
+        # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
+        img_final_bin = cv.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
+        img_final_bin = cv.erode(~img_final_bin, kernel, iterations=2)
+        (thresh, img_final_bin) = cv.threshold(img_final_bin, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+   
+        # Find contours for image, which will detect all the boxes
+        im2, contours, hierarchy = cv.findContours(
+            img_final_bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        # Sort all the contours by top to bottom.
+        (contours, boundingBoxes) = self.sort_contours(contours, method="top-to-bottom")
+        
+        threshed_height = self.imgHeight *0.3
+        threshed_width = self.imgWidth * 0.3
+
+        featureArray = []
+
+        for c in contours:
+            # Returns the location and width,height for every contour
+            x, y, w, h = cv.boundingRect(c)
+            cv.rectangle(self.image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            contour_area = w*h
+            area_ratio = contour_area / self.getImageArea()
+            featureArray.append(area_ratio)
+        
+        return featureArray
+
+def writeToFile (toWrite):
+    resultArea = open('resultShape.txt', 'w')
+    resultingArea.write(str(the_features))
+    resultingArea.write('\n')
+    resultArea.close()
+
+if __name__ == "__main__":    
+    img_name = "Images/" + str(1) + ".jpg"
+
+    shape = Shapes(img_name)
+    the_features = shape.boxFeatures()
+
+    writeToFile(the_features)
+            
+    
 
