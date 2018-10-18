b8399e7182e6ba0363c3f4f5eff1918fa363cb5f
unknown <ykyio1@student.monash.edu>
2018-10-18 09:34:25 +0800

    Very The Final! Freaking Good Dy. Added my own way of doing it instead of copy whole thing. Commments added too

--- shapes/Method2.py
+++ shapes/Method2.py
@@ -5,32 +5,37 @@ import numpy as np
 class Shapes:
 
     def __init__ (self):
-        self.image = None;
-        self.imgHeight = None;
-        self.imgWidth = None;
+        self.image = None;          # Original Image
+        self.imgHeight = None;      # Original Image's Height
+        self.imgWidth = None;       # Original Image's Width
 
     def getImageHeightWidth (self, image):
-        # Get the Height and width of the original image
+        # Get the Height and width of the original image and save it
         height, width = image.shape[:2]
         return height, width
 
+    def getImageArea(self):
+        # Returns the area of the original image
+        img_area = self.imgHeight * self.imgWidth
+        return img_area
+
+    def medianBlurImage (self, image):
+        # Remove noise. Blur the Image
+        kernel_size = 3
+        median_blur = cv.medianBlur(image, kernel_size)
+        return median_blur
+    
     def binarizeImage (self, image):
-        # Binarizing the grayscaled image using otsu. Since we have different kind of images, trial and error for the best threshold for all images
-        #       is not ideal here. So, otsu is used instead to allow an automatic calculation of a threshold value from an image histogram into bimodal image.
+        # Binarizing the grayscaled image using otsu. Since we have different kind of images, trial and error for the best threshold for all images is not ideal here. 
+        # So, otsu is used instead to allow an automatic calculation of a threshold value from an image histogram into bimodal image.
         (thresh, binarized) = cv.threshold(image, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
         return binarized
 
     def invertImage (self, image):
         # Invert the image colour. Input image have to be black and white
-        # Since FindContours treats white as foreground and black as blackground, we invert it for a better detection.
+        # Since openCV findContours function treats white as foreground and black as blackground, we invert it for a better detection.
         inv_img = 255 - image
         return inv_img
-
-    def medianBlurImage (self, image):
-        # Remove noise. Blur Image
-        kernel_size = 3
-        median_blur = cv.medianBlur(image, kernel_size)
-        return median_blur
 
     def preProcess(self):
         # Preprocess the image for shape detection.
@@ -40,71 +45,80 @@ class Shapes:
         img_inverted = self.invertImage(img_binarized)
         return img_inverted
 
-    def getImageArea(self):
-        # Returns the area of the original image
-        img_area = self.imgHeight * self.imgWidth
-        return img_area
-
-    def detectHoriLines(self, img, kernel):
-        # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
-        hori_kernel = cv.getStructuringElement(cv.MORPH_RECT, (kernel, 1))
-        
-        # Morphological operation to detect horizontal lines from an image
-        img_temp2 = cv.erode(img, hori_kernel, iterations=3)
-        hori_img = cv.dilate(img_temp2, hori_kernel, iterations=3)
+    def morphHoriLines(self, img):
+        # This function performs a morphological operation HORIZONTALLY on the image
+        #   Extracting only Lines that are detected to be horizontal occurding to the specified kernel 
+        hori_kernel = self.imgWidth // 30
+   
+        # Create structure element for extracting horizontal lines through morphology operations
+        horizontalStructure = cv.getStructuringElement(cv.MORPH_RECT, (hori_kernel, 1))
+    
+        # Apply morphology operations
+        hori_img = cv.erode(img, horizontalStructure)
+        hori_img = cv.dilate(hori_img, horizontalStructure)
+ 
         return hori_img
 
+    def morphVertLines(self, img):
+        # This function performs a morphological operation VERTICALLY on the image
+        #   Extracting only Lines that are detected to be vertical occurding to the specified kernel 
+        vert_kernel = self.imgHeight // 30
+    
+        # Create structure element for extracting vertical lines through morphology operations
+        verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, vert_kernel))
 
-    def detectVertLines(self, img, kernel):
-        # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
-        verticle_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernel))
+        # Apply morphology operations
+        vert_img = cv.erode(img, verticalStructure)
+        vert_img = cv.dilate(vert_img, verticalStructure)
 
-        # Morphological operation to detect verticle lines from an image
-        img_temp1 = cv.erode(img, verticle_kernel, iterations=3)
-        vert_img = cv.dilate(img_temp1, verticle_kernel, iterations=3)
         return vert_img
 
     def combineHoriVert(self, horizontal_img, vertical_img):
-        # A kernel of (3 X 3) ones.
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+        # Combining both vertical image and horizontal image
+        #   To allow a better detection on only the vertical and horizontal lines of the box
+        
+        # Inverting the image to fit in the findContour function later on
+        adding = ~cv.add(vertical_img, horizontal_img)
+        
+        return adding
 
-        # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
-        alpha = 0.5
-        beta = 1.0 - alpha
-
-        # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
-        combined = cv.addWeighted(vertical_img, alpha, horizontal_img, beta, 0.0)
-        combined = cv.erode(~combined, kernel, iterations=2)
-        (thresh, combined) = cv.threshold(combined, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-
-        return combined
-
-    def getBoxFeatures(self, img, output_limit = 8):
+    def featuresExtraction(self, img, output_limit = 8):
         # Find contours for image, which will detect all the boxes
         # Find all the contours
         im2, contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         
-        # Sort all the contours by the largest to the smallest
+        # Sort all the contours by the largest to the smallest area
         contour = sorted(contours, key=cv.contourArea, reverse = True)
 
-        threshed_height = self.imgHeight *0.3
-        threshed_width = self.imgWidth * 0.3
+        # Define all the threshold of the box we do not want to detect. These boxes appear due to noises or uneccessary stuff
+        smallest_threshed_height = self.imgHeight *0.2
+        smallest_threshed_width = self.imgWidth * 0.2
 
+        # Putting all the features into the array
         featureArray = []
 
+        # Looping through all contours to get the features
         for c in contour:
-            # Returns the location and width,height for every contour
+            # Returns the location, width and height
             x, y, w, h = cv.boundingRect(c)
-            cv.rectangle(self.image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv.imshow('rect', self.image)
-            cv.waitKey(0)
+            contour_area = w*h      # Gets the area of the contour
+            image_area = self.getImageArea()    # Get the area of the image
 
-            contour_area = w*h
-            area_ratio = contour_area / self.getImageArea()
-            featureArray.append(area_ratio)
+            # If the contour detected is not the border of the image, continue on
+            if (contour_area != image_area):
+                # Threshold that was defined previously
+                if (w >= smallest_threshed_width and h >= smallest_threshed_height ):
+                    cv.rectangle(self.image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    # Get the area ratio as the data for Machine Learning later on
+                    area_ratio = contour_area / image_area
+                    # Put it into the area
+                    featureArray.append(area_ratio)
+
+            # If there are more features detected then we just stop, cause we dont want more
             if len(featureArray) == output_limit:
                 break;
 
+        # If we detected way less contour as usual, then we append 0's to make up for the space
         while len(featureArray) < output_limit:
             featureArray.append(0);
 
@@ -116,17 +130,19 @@ class Shapes:
         self.imgHeight = self.image.shape[0]
         self.imgWidth = self.image.shape[1]
 
+        # Perform a pre-process on the image
         processed = self.preProcess()
 
-        # Defining a kernel length
-        kernel_length = np.array(self.image).shape[1]//40
+        # Both morphological horizontal lines and vertical lines are taken from openCV's website. The following is the source
+        # source: https://docs.opencv.org/3.4/dd/dd7/tutorial_morph_lines_detection.html
+        hori_img = self.morphHoriLines(processed)
+        vert_img = self.morphVertLines(processed)
 
-        hori_img = self.detectHoriLines(processed, kernel_length)
-        vert_img = self.detectVertLines(processed, kernel_length)
-
+        # Perform a combination of both vertical image and horizontal image.
         combined_img = self.combineHoriVert(hori_img, vert_img)        
 
-        array_features = self.getBoxFeatures(combined_img)
+        # Get the features
+        array_features = self.featuresExtraction(combined_img)
 
         return array_features
         
@@ -140,12 +156,15 @@ def writeToFile (toWrite):
 
 if __name__ == "__main__":
     currentDir = os.path.dirname(os.path.realpath(__file__));
+
+    
     img_name = "Images/" + str(1) + ".jpg"
     img_path = os.path.join(currentDir, img_name )
 
     image = cv.imread(img_path)
     shape = Shapes();
     the_features = shape.boxFeatures(image)
+    cv.destroyAllWindows()
 
     writeToFile(the_features)
 
